fc99ecf70bba42487d016d37a8190f213c4dba97
Asatashi <76907974+Asatashi@users.noreply.github.com>
2021-03-23 14:51:58 +0100

    Add files via upload

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ package tictactoe;
 
 
 import java.util.Arrays;
-import java.util.InputMismatchException;
+import java.util.InputMismatchException;            //now you can chose at the start if u want to play vs bot or user or maybe watch bots fight each other
 import java.util.Scanner;
 
 public class Main {
@@ -58,7 +58,7 @@ public class Main {
     }
 
 
-    public static void displayTable(String[][] table) {       //it displays the game board with user input
+    public static void displayTable(String[][] table,String commandTwo, String commandOne) {       //it displays the game board with user input
         Scanner scanner = new Scanner(System.in);
         int firstInput;
         int secondInput;
@@ -72,8 +72,20 @@ public class Main {
         }
         String os = "O ";
         String xs = "X ";
-            if ((count == 1 || count == 3 || count == 5 || count == 7 || count == 9)) {
-                boolean valid = false;
+        if ((count == 1 || count == 3 || count == 5 || count == 7 || count == 9)) {
+            boolean valid = false;
+            if (commandOne.equals("easy")) {
+                System.out.println("Making move level \"easy\"");
+                while (!valid) {
+                    int randomNumOne = (int) (Math.random() * 3 + 1);
+                    int randomNumTwo = (int) (Math.random() * 3 + 1);
+                    if (table[randomNumOne - 1][randomNumTwo - 1].equals("  ")) {
+                        table[randomNumOne - 1][randomNumTwo - 1] = xs;
+                        rawTable(table);
+                        valid = true;
+                    }
+                }
+            } else {
                 while (!valid) {
                     System.out.print("Enter the coordinates: > ");
                     try {
@@ -93,7 +105,8 @@ public class Main {
                         System.out.println("Coordinates should be from 1 to 3!");
                     }
                 }
-            } else {
+            }
+            } else if(commandTwo.equals("easy")) {
                 System.out.println("Making move level \"easy\"");
                 boolean valid = false;
                 while (!valid) {
@@ -105,7 +118,28 @@ public class Main {
                         valid = true;
                     }
                 }
+            } else {
+            boolean valid = false;
+            while (!valid) {
+                System.out.print("Enter the coordinates: > ");
+                try {
+                    firstInput = scanner.nextInt();
+                    secondInput = scanner.nextInt();
+                    if (table[firstInput - 1][secondInput - 1].equals("  ")) {
+                        table[firstInput - 1][secondInput - 1] = os;
+                        rawTable(table);
+                        valid = true;
+                    } else {
+                        System.out.println("This cell is occupied! Choose another one!");
+                    }
+                } catch (InputMismatchException e) {
+                    System.out.println("You should enter numbers!");
+                    return;
+                } catch (IndexOutOfBoundsException e) {
+                    System.out.println("Coordinates should be from 1 to 3!");
+                }
             }
+        }
             gameRules(table, os, xs, count);
         }
 
@@ -116,25 +150,40 @@ public class Main {
         for (String[] strings : table) {
             Arrays.fill(strings, s);
         }
-        String stringStart = "";
-        String commandOne = "";
-        String commandTwo = "";
+        String stringStart;
+        String commandOne;
+        String commandTwo;
         String correctStart = "start";
         String userCorrect = "user";
         String aiCorrect = "easy";
         boolean valid = false;
         while (!valid) {
-            System.out.println("Input command: > ");
+            System.out.print("Input command: > ");
             stringStart = scanner.next();               //TODO make methods depending on the start input with determines if a user(s) play or bot(s)
-            commandOne = scanner.next();
+            commandOne = scanner.next();                //TODO make only one input
             commandTwo = scanner.next();
+
             if (stringStart.equals(correctStart)
                     && (commandOne.equals(userCorrect) || commandOne.equals(aiCorrect))
                     && (commandTwo.equals(userCorrect) || commandTwo.equals(aiCorrect))) {
-                rawTable(table);
-                for (int i = 0; i < 9; i++) {
-                    displayTable(table);
-                    valid = true;
+                if (commandOne.equals(userCorrect) && commandTwo.equals(aiCorrect)) {
+                    rawTable(table);
+                    for (int i = 0; i < 9; i++) {
+                        displayTable(table,commandTwo,commandOne);
+                        valid = true;
+                    }
+                } else if (commandOne.equals(aiCorrect) && commandTwo.equals(aiCorrect)) {
+                    rawTable(table);
+                    for (int i = 0; i < 9; i++) {
+                        displayTable(table,commandTwo,commandOne);
+                        valid = true;
+                    }
+                } else {
+                    rawTable(table);
+                    for (int i = 0; i < 9; i++) {
+                        displayTable(table,commandTwo,commandOne);
+                        valid = true;
+                    }
                 }
             } else {
                 System.out.println("Bad parameters!");
